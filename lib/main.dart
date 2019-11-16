cd82c5fa9fda8d8faade14476747c936c5511ce7
otopba <otopba@inbox.ru>
2019-11-16 19:18:52 +0200

    landscape

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:dashboardx/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:preferences/preference_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations(
+    [DeviceOrientation.landscapeLeft],
+  );
+  await SystemChrome.setEnabledSystemUIOverlays([]);
   await PrefService.init(prefix: 'pref_');
   runApp(App());
 }
